60690006f0f059b2237bd9c9d0b137eadea23ac3
yangara <yangara@codeblock.co.in>
2024-08-27 10:12:19 +0530

    feat: :sparkles: simple table scan

--- src/hello.py
+++ src/hello.py
@@ -1,10 +1,27 @@
 import json
+import boto3
+from os import environ
+from botocore.exceptions import ClientError
 
 def handler(event, context):
-    body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
-    }
+    dynamodb = boto3.resource('dynamodb')
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    table_name = environ.get('USERS_TABLE')
+    if not table_name:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "USERS_TABLE environment variable not set"})
+        }
+
+    table = dynamodb.Table(table_name)
+    
+    try:
+        table_response = table.scan()
+        response = {"statusCode": 200, "body": json.dumps(table_response)}
+    except ClientError as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
 
     return response
